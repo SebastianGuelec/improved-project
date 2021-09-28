dd1f3c923d5c16b3731fd25600665ca8a0c1ce45
Sebastian <sebastian@Sebastians-MacBook-Pro.local>
2021-09-28 22:48:02 +0200

    UserAtuh added

--- textify-backend/src/main/java/com/textify/textify/entity/User.java
+++ textify-backend/src/main/java/com/textify/textify/entity/User.java
@@ -2,16 +2,27 @@ package com.textify.textify.entity;
 
 import com.sun.istack.NotNull;
 import lombok.Data;
+
 import org.springframework.data.annotation.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Transient;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import java.io.Serial;
+import java.util.Collection;
 
 @Data
 @Entity
-public class User {
+public class User implements UserDetails {
+
+    @Serial
+    private static final long serialVersionUID = 4071234728582967483L;
+
 
     @Id
     @GeneratedValue
@@ -30,5 +41,35 @@ public class User {
     @Size(min = 8, max=255)
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
     private String password;
+
+    @Override
+    @Transient
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return AuthorityUtils.createAuthorityList("Role_USER");
+    }
+
+    @Override
+    @Transient
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    @Transient
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    @Transient
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    @Transient
+    public boolean isEnabled() {
+        return true;
+    }
 }
 
