9bec072cc518ddef5c97249ea8eeaba1fd2fb4c4
Sebastian <sebastian@Sebastians-MacBook-Pro.local>
2021-09-28 22:04:50 +0200

    Minmaxlength added

--- textify-backend/src/main/java/com/textify/textify/entity/User.java
+++ textify-backend/src/main/java/com/textify/textify/entity/User.java
@@ -1,10 +1,14 @@
 package com.textify.textify.entity;
 
+import com.sun.istack.NotNull;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
@@ -14,10 +18,17 @@ public class User {
     @GeneratedValue
     private Long id;
 
+    @NotNull
+    @Size(min = 4, max=255)
     private String username;
 
+    @NotNull
+    @Size(min = 4, max=255)
     private String nickname;
 
+    @NotNull
+    @Size(min = 8, max=255)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
     private String password;
 }
 
