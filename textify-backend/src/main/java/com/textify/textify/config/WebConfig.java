6e060af426b371a47f9c2d5fc144e8b15b7c1651
Sebastian <sebastian@Sebastians-MacBook-Pro.local>
2021-09-29 09:49:07 +0200

    super basic ressource management

--- textify-backend/src/main/java/com/textify/textify/config/WebConfig.java
+++ textify-backend/src/main/java/com/textify/textify/config/WebConfig.java
@@ -3,14 +3,25 @@ package com.textify.textify.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
     AppConfig appConfig;
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:" + appConfig.getUploadPath() + "/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
+    }
 
     @Bean
     CommandLineRunner createUploadFolder() {
